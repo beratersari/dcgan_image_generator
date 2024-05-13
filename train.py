9f31591481e8ed61e0b777693573e99eac82172d
Berat Ersari <56562278+beratersari@users.noreply.github.com>
2024-05-13 23:38:24 +0300

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import time
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 import numpy as np
-from glob import glob
+import glob
 import datetime
 import random
 from PIL import Image
